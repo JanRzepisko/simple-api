8be3323bf66ca82dad2ff76f96e6545a7a411080
janrzepisko <rzejan@gmail.com>
2023-11-24 12:44:31 +0100

    tests

--- simpleapi.example/Actions/ExampleEndpoint.cs
+++ simpleapi.example/Actions/ExampleEndpoint.cs
@@ -22,6 +22,7 @@ public static class ExampleEndpoint
         
         public Task<int> Handle(Command command)
         {
+            
             return Task.FromResult(_exampleService.AddTwo()) ;
         }
     }
